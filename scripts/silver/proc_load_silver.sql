9dfcbd6bf15230bb29ac8f5d3cec97982691ec2c
marceltrnavsky1 <marcel.trnavsky1@gmail.com>
2026-02-20 15:36:22 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -82,3 +82,44 @@ SELECT
 		AS DATE
 	) AS prd_end_dt -- Calculate end date as one day before the next start date
 	FROM bronze.crm_prd_info
+
+
+
+;
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+)
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	 END AS sls_order_dt,
+CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	 END AS sls_ship_dt,
+CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+	 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	 END AS sls_due_dt,
+	 CASE WHEN sls_sales IS NULL OR sls_sales <=0 OR sls_sales != sls_quantity * ABS(sls_price)
+	THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+END AS sls_sales,
+sls_quantity,
+CASE WHEN sls_price IS NULL OR sls_price <=0
+	THEN sls_sales / NULLIF(sls_quantity, 0)
+	ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details
+-- WHERE sls_ord_num != TRIM(sls_ord_num)									CHECKING
+-- WHERE sls_prd_key NOT IN (SELECT prd_key FROM silver.crm_prd_info)		CHECKING
+-- WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info)		CHECKING
